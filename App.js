e221701cb0d553852816d9b7a6e5c31f8b4fa7b4
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-09-30 18:43:36 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+
+import episodes from './src/episodes';
+import TrackPlayer from 'react-native-track-player';
 
 export default function App() {
+
+  useEffect(() => {
+    TrackPlayer.setupPlayer().then(async () => {
+      // The player is ready to be used
+      console.log('Player ready');
+      console.log(episodes)
+      // add the array of songs in the playlist
+      await TrackPlayer.reset();
+      await TrackPlayer.add(episodes[1]);
+      TrackPlayer.play();
+
+      //add listener on track change
+    })
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <FlatList
+        data={episodes}
+        keyExtractor={(item, index) => item.id}
+        renderItem={itemData => (
+          <TouchableOpacity onTouch={() => TrackPlayer.stop()}>
+            <View style={styles.todoItem}>
+              <Text>{itemData.item.title}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  todoItem: {
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 2,
+    padding: 10
+  }
 });
---
+++ src/episodes.js
@@ -0,0 +1,25 @@
+const episodes =  [
+  {
+    id: "1",
+    title: "Case #2 Britney",
+    artist: "Mystery Show",
+    url: "https://traffic.megaphone.fm/GLT5025099642.mp3?updated=1511216722",
+    artwork: "https://is3-ssl.mzstatic.com/image/thumb/Podcasts123/v4/94/aa/a1/94aaa142-279f-aec6-2634-436c4b28cfac/mza_8970625345940320927.png/600x600bb.png"
+  },
+  {
+    id: "2",
+    title: "Mr. Hollowell Didn't like that",
+    artist: "Revisionist History",
+    url: "https://leopard.megaphone.fm/PPY4382928573.mp3?updated=1502394887",
+    artwork: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/10/b4/f9/10b4f94c-c484-7dcd-8c40-10b142b42ab9/mza_6334207016816396690.jpg/600x600bb.png"
+  },
+  {
+    id: "3",
+    title: "Fargo",
+    publisher: "Snap Judgement",
+    url: "https://www.podtrac.com/pts/redirect.mp3/audio.wnyc.org/snapjudgment/snapjudgment180315_snapclassic_fargo.mp3",
+    artwork: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts118/v4/ed/4c/d0/ed4cd05c-357a-ef54-b97c-89e9257ec967/mza_1200562078040939068.png/600x600bb.png"
+  }
+];
+
+export default episodes;
